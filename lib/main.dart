06140a44de4e62f9fdcb59620ce795239129f974
danielchocho97 <dmchochog@utn.edu.ec>
2024-06-09 22:39:51 -0500

    Hola soy Daniel

--- lib/main.dart
+++ lib/main.dart
@@ -7,17 +7,27 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        title: 'Flutter Demo',
-        theme: ThemeData(
-          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-          useMaterial3: true,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text("Hola mundo"),
+          titleTextStyle: const TextStyle(
+            color: Colors.white, // Color del texto
+            fontSize: 20, // Tamaño del texto
+            fontWeight: FontWeight.bold, // Grosor del texto
+          ),
         ),
-        home: AppBar(
-          title: Text("hola mundo"),
-        ));
+        body: const Center(
+          child: Text("Hola soy Daniel Chocho"),
+        ),
+      ),
+    );
   }
 }
